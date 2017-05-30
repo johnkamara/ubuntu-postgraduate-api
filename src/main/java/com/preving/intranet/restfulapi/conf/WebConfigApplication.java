0f375c9f83329f5781a2ab15155e63b2f99ed88c
Rogelio Gragera <r.gragera@preving.com>
2017-05-30 21:21:06 +0200

    UPDATED - Quitar configuración swagger

--- src/main/java/com/preving/intranet/restfulapi/conf/WebConfigApplication.java
+++ src/main/java/com/preving/intranet/restfulapi/conf/WebConfigApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 @EnableWebMvc
-//@Profile("preprod")
 public class WebConfigApplication extends WebMvcConfigurerAdapter {
 
     @Override
