e99352829d7823d5b24822f5a6a3c904f08fa2ca
Rogelio Gragera <r.gragera@preving.com>
2017-06-01 22:47:29 +0200

    UPDATED - Se buscan los usuarios en un List en lugar de buscar por variables definidas en el yml.

--- src/main/java/com/preving/intranet/restfulapi/security/service/JwtUserDetailsServiceImpl.java
+++ src/main/java/com/preving/intranet/restfulapi/security/service/JwtUserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.preving.intranet.restfulapi.security.service;
 
 import com.preving.intranet.restfulapi.model.domain.User;
 import com.preving.intranet.restfulapi.security.JwtUserFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,14 +17,16 @@ import java.util.List;
 @Service
 public class JwtUserDetailsServiceImpl implements UserDetailsService {
 
-    // todo fj2m autowire usuarios usuario, salto, hash.... no guardar la contraseña como texto plano si se puede
-    private List<User> usuarios;
+     @Autowired
+     private List<User> usuarios;
 
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
 
-        // todo fj2m buscar el usuario en usuarios para hacerlo sencillo
-        User user = new User((long) 1, "roge", "r.gragera@preving.com", "Roge", "Gragera", "pwd", true, null);
+        User user = usuarios.stream()
+            .filter(e -> e.getUsername().equals(username))
+            .findFirst()
+            .orElse(null);
 
         if (user == null) {
             throw new UsernameNotFoundException(String.format("No existe ningun usuario con el nombre de usuario: '%s'.", username));
