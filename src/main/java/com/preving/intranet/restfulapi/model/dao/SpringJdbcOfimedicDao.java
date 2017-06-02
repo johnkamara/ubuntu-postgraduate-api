23c6fa3e10650a69676a2d1880c643dadc4432d7
Felips Gonzalez <felipe.gonzalez@preving.com>
2017-06-02 21:11:28 +0200

    ADDED: Clase que implementa la interfaz OfimedicDao para consultas a la BD SQL Server

---
+++ src/main/java/com/preving/intranet/restfulapi/model/dao/SpringJdbcOfimedicDao.java
@@ -0,0 +1,12 @@
+package com.preving.intranet.restfulapi.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by felips on 2/6/17.
+ */
+@Repository
+public class SpringJdbcOfimedicDao {      // todo implements OfimedicDao {
+
+    // TODO: 2/6/17  FGG - Implementar los métodos de la interfaz, consultas sql server
+}
